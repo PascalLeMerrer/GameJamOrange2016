aa9f1078f6d7160ec25b030d8286c5cfc8e8c98e
Pascal Le Merrer <pascal.lemerrer@orange.com>
2016-10-18 17:29:37 +0200

    Fix preloader

--- js/preloader.js
+++ js/preloader.js
@@ -4,22 +4,20 @@ Jumpup.Preloader = function (game) {
     this.logo = null;
     this.preloadBar = null;
     this.ready = false;
+    this.context = {};
 };
 
 Jumpup.Preloader.prototype = {
 
     init: function () {
-
-        this.add.sprite(265, 400, 'logo');
+        // this.add.sprite(265, 400, 'logo');
         this.fontLoaded = false;
-
     },
 
     preload: function () {
 
         this.preloadBar = this.add.sprite(120, 260, 'preload');
         this.load.setPreloadSprite(this.preloadBar);
-        //this.load.image('trees', 'assets/trees.png');
         this.load.image('background', 'assets/Full-Background.png');
         //this.load.audio('levelup', 'assets/243020__plasterbrain__game-start.ogg');
 
@@ -38,22 +36,17 @@ Jumpup.Preloader.prototype = {
 
         if (!this.ready)
         {
+            if (this.game.fontLoaded)
+            {
+                this.ready = true;
 
-            this.state.start('MainMenu', true, false, context);
-
-            // if (this.cache.isSoundDecoded('coin') &&
-            //     this.cache.isSoundDecoded('jumping') &&
-            //     this.game.fontLoaded)
-            // {
-            //     this.ready = true;
-
-            //     var context = {
-            //         score: 0,
-            //         level: 1,
-            //         lives: 3
-            //     }
-            //     this.state.start('MainMenu', true, false, context);
-            // }
+                var context = {
+                    score: 0,
+                    level: 1,
+                    lives: 3
+                }
+                this.state.start('MainMenu', true, false, this.context);
+            }
         }
 
     }
