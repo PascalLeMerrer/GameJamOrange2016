03abbe8628833d48edec4cdd365c6fbdb4d4f025
Pascal Le Merrer <pascal.lemerrer@orange.com>
2016-10-18 18:03:11 +0200

    Detect key press and play sound

--- js/game.js
+++ js/game.js
@@ -22,26 +22,21 @@ Jumpup.Game.prototype = {
 
         this.world.resize(800, 600);
 
+
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
         this.physics.arcade.gravity.y = 750;
         this.physics.arcade.skipQuadTree = false;
-
-
-
-
-
     },
 
     create: function () {
         // this.physics.arcade.enable(this.player);
 
         this.background = this.add.tileSprite(0, 0, 800, 600, 'background');
+
         var style = { fill: "#ffffff", align: "center", fontSize: 32 };
 
         this.scoreText = this.createText(20, 20, this.context.score || '000', style);
-
-
 
         this.keys = this.add.group();
         this.keys.enableBody = true;
@@ -53,6 +48,17 @@ Jumpup.Game.prototype = {
 
         this.keys.add(k.sprite);
         this.keys.add(b.sprite);
+
+        this.initKeyboard();
+    },
+
+    initKeyboard: function() {
+        game.input.keyboard.addCallbacks(this, null, null, this.onPress);
+    },
+
+    onPress: function(char) {
+        this.sound.play('key');
+        console.log(char);
     },
 
     createText: function(x, y, text, style, size)
@@ -72,6 +78,7 @@ Jumpup.Game.prototype = {
         }, null, this);
 
 
+
     },
 
     end: function(){
@@ -87,16 +94,16 @@ function Key(game, x, y, keyLetter){
     this.sprite = game.add.sprite(x, y, 'key');
     this.sprite.width = 50;
     this.sprite.height = 50;
-    
+
     // Letter
-    var style = { 
-        font: "32px Arial", fill: "#ff6600", 
-        wordWrap: true, wordWrapWidth: this.sprite.width, 
+    var style = {
+        font: "32px Arial", fill: "#ff6600",
+        wordWrap: true, wordWrapWidth: this.sprite.width,
         align: "center"
     };
     var letterText = this.sprite.addChild(game.make.text(30, 25, keyLetter, style));
     letterText.anchor.set(0.5);
-    
+
     // Background physics body
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
@@ -104,4 +111,4 @@ function Key(game, x, y, keyLetter){
     this.sprite.body.collideWorldBounds = true;
     this.sprite.body.setSize(44, 44, 3, 3)
 
-} 
+}
